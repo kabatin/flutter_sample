bc5e4e607e8b918523e8fb75f5d6a496645ae94e
Hiroshi Kabayama <kabatin@gmail.com>
2021-06-12 00:57:22 +0900

    feat: added Networking

--- lib/page_networking.dart
+++ lib/page_networking.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:test_app/layout_type.dart';
 
 class PageNetworking extends StatefulWidget implements HasLayoutGroup {
@@ -15,6 +17,20 @@ class PageNetworking extends StatefulWidget implements HasLayoutGroup {
 }
 
 class _PageNetworkingState extends State<PageNetworking> {
+  String result = '';
+
+  Future<void> httpGet() async {
+    var url = Uri.http('210.129.22.215', 'getip.php');
+    await http.get(url).then((res) => {
+          if (res.statusCode != HttpStatus.ok)
+            throw Exception('StatusCode=' + res.statusCode.toString())
+          else
+            setState(() {
+              result = res.body;
+            })
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,14 +40,21 @@ class _PageNetworkingState extends State<PageNetworking> {
         body: Container(
             constraints: const BoxConstraints.expand(),
             color: Colors.white,
-            child: Row(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
+                ElevatedButton(
+                    onPressed: () {
+                      httpGet().catchError((err) {
+                        setState(() => {result = err.toString()});
+                      });
+                    },
+                    child: Text('API Call', style: TextStyle(fontSize: 40.0))),
                 Text(
-                  'Coming soon...',
-                  style: const TextStyle(fontSize: 40.0),
+                  '\nCaution:\nNot woking on web browser.\nreason is cors.\n\nResult: $result',
+                  style: const TextStyle(fontSize: 30.0),
                 )
               ],
             )));
